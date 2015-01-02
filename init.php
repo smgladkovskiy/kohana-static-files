657f28570f0f8bd430fb33941e14672e480c3d5c
SMGladkovskiy <smgladkovskiy@gmail.com>
2015-01-02 14:30:31 +0300

    Composer update

--- init.php
+++ init.php
@@ -1,5 +1,18 @@
 <?php defined('SYSPATH') or die('No direct script access.');
 
+/*
+|--------------------------------------------------------------------------
+| Register The Composer Auto Loader
+|--------------------------------------------------------------------------
+|
+| Composer provides a convenient, automatically generated class loader
+| for our application. We just need to utilize it! We'll require it
+| into the script here so that we do not have to worry about the
+| loading of any our classes "manually". Feels great to relax.
+|
+*/
+require __DIR__.'vendor/autoload.php';
+
 /**
  * @package Kohana-static-files
  */
